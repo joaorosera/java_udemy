22bddc08ce4f90d92c4f01e22f1d6a243079686b
João Vitor Rosera <jvrosera01@gmail.com>
2025-03-13 16:47:39 -0300

    Exercícios While

---
+++ Main14.java
@@ -0,0 +1,81 @@
+package curso_programacao;
+
+import java.util.Scanner;
+
+public class Main14 {
+
+    public static void main(String[] args) {
+
+        // Exercício 1: Programa para validar uma senha até que seja correta
+
+       /* int senhaCorreta = 2002; // 
+
+        Scanner sc = new Scanner(System.in); 
+
+        int tentarSenha; 
+
+        System.out.println("Tente adivinhar a senha!"); 
+
+        while (true) { 
+            tentarSenha = sc.nextInt(); 
+            
+            if (tentarSenha == senhaCorreta) { 
+                System.out.println("Acesso permitido!"); 
+                break; // Sai do loop
+            } else {
+                System.out.println("Senha inválida!"); 
+            }
+        }
+
+        sc.close(); */
+        
+    	// Exercício 2:
+    	
+    	/*Scanner sc = new Scanner(System.in);
+    	
+    	int alcool = 0;
+    	int gasolina = 0;
+    	int diesel = 0;
+    	
+    	int tipo = sc.nextInt();
+    	
+    	while (tipo != 4) {
+    		if (tipo == 1) {
+    			alcool += alcool + 1;
+    		}
+    		else if (tipo == 2) {
+    			gasolina += gasolina + 1;
+    		}
+    		else if (tipo == 3) {
+    			diesel += diesel +1;
+    		}
+    		
+    		tipo = sc.nextInt();
+    }
+    	
+    	System.out.println("Muito obrigado!");
+    	System.out.println("Alcool " + alcool);
+    	System.out.println("Gasolina " + gasolina);
+    	System.out.println("Diesel " + diesel); */
+    	
+    	// Exercício 3
+    	
+    	/* int i = 1;
+    	
+    	while (i <= 10) {
+    		System.out.println(i);
+    		i++;
+    	} */
+    	
+    	// Exercício 4
+    	
+    	int j = 0;
+    	
+    	while (j <= 25) {
+    		System.out.println(j);
+    		j++;
+    	}
+    	
+
+    }
+}
